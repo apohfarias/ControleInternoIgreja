272cdec31c5ba2416600cceb0cd8610960a8a0bf
unknown <apoh@TI08.iito>
2018-03-23 16:16:39 -0300

    Criado package DAO

---
+++ src/br/com/cii/dao/EstadoDAO.java
@@ -0,0 +1,9 @@
+package br.com.cii.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import br.com.cii.model.Estado;;
+
+public class EstadoDAO {
+
+}
--- src/br/com/cii/model/Estado.java
+++ src/br/com/cii/model/Estado.java
@@ -1,9 +1,32 @@
 package br.com.cii.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="estado")
 public class Estado {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private int idEstado;
+	@Column(name="nome")
 	private String nome;
 	
+		
+	public Estado() {
+		
+	}
+
+	public Estado(int idEstado, String nome) {
+		super();
+		this.idEstado = idEstado;
+		this.nome = nome;
+	}
 	
 	public int getIdEstado() {
 		return idEstado;
