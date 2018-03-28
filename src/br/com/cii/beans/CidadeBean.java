34d6b2144c07d849a16751f0633fc635bf9acf35
unknown <apoh@TI08.iito>
2018-03-28 16:20:15 -0300

    Adicionado bootstrap

--- src/br/com/cii/beans/CidadeBean.java
+++ src/br/com/cii/beans/CidadeBean.java
@@ -1,7 +1,10 @@
 package br.com.cii.beans;
 
+import br.com.cii.dao.CidadeDAO;
 import br.com.cii.dao.Conexao;
+import br.com.cii.dao.EstadoDAO;
 import br.com.cii.model.Cidade;
+import br.com.cii.model.Estado;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -12,110 +15,16 @@ import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 @ManagedBean
-@ApplicationScoped
+@ViewScoped
 public class CidadeBean {
-	// Listar cidade
-	private List<Cidade> cidades = new ArrayList<Cidade>();
-	// Instanciando nova cidade
-	private Cidade cidade = new Cidade();
-	// Instanciando conexao
-	private Connection conn;
-	// Instanciando comando
-	private Statement comando;
-
-	public List<Cidade> getCidades() {
-		return cidades;
-	}
-
-	public void setCidades(List<Cidade> cidades) {
-		this.cidades = cidades;
-	}
-
-	public Cidade getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(Cidade cidade) {
-		this.cidade = cidade;
-	}
-
-	public Connection getConn() {
-		return conn;
-	}
-
-	public void setConn(Connection conn) {
-		this.conn = conn;
-	}
-
-	public Statement getComando() {
-		return comando;
-	}
-
-	public void setComando(Statement comando) {
-		this.comando = comando;
-	}
-
-	public CidadeBean() {
-		buscarTodos();
-	}
-
-	public void buscarTodos() {
-		conectar();
-		ResultSet rs;
-		cidades = new ArrayList<>();
-		try {
-			rs = comando.executeQuery("SELECT * FROM cidade");
-			while (rs.next()) {
-				Cidade temp = new Cidade();
-
-				temp.setIdCidade(rs.getInt("idCidade"));
-				temp.setNome(rs.getString("nome"));
-				// temp.setEstado(rs.getString("estado"));
-
-				cidades.add(temp);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void conectar() {
-		// TODO Auto-generated method stub
-		try {
-			conn = Conexao.getConexao();
-			comando = conn.createStatement();
-			System.out.println("Conectado!");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-
-	public void inserir() {
-
-	}
-
-	public void apagar() {
-
-	}
-
-	public void apagarPorId() {
-
-	}
 	
-
-	public void fechar() {
-		try {
-			comando.close();
-			conn.close();
-			System.out.println("Conexão Fechada");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
+	private Cidade cidade;
+	private CidadeDAO cidadeDao;
+	private List<Cidade> cidades;
+	
+	
 }
--- src/br/com/cii/model/Cidade.java
+++ src/br/com/cii/model/Cidade.java
@@ -1,8 +1,23 @@
 package br.com.cii.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="cidade")
 public class Cidade {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private int idCidade;
+	@Column(name="nome")
 	private String nome;
+	@Column(name="estado")
 	private Estado estado;
 	
 	public int getIdCidade() {
